231de765d1e63d588048d64e954e9d1d55142063
Difrex <difrex.punk@gmail.com>
2016-08-04 11:03:49 +0300

    --config option and some PEP8 and comments

--- surok.py
+++ surok.py
@@ -8,8 +8,19 @@ from surok.templates import gen
 from surok.discovery import resolve
 from surok.system import reload_conf
 
+import argparse
+
 # Load base configurations
-f = open('/etc/surok/conf/surok.json', 'r')
+surok_conf = '/etc/surok/conf/surok.json'
+
+# Command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--config')
+args = parser.parse_args()
+if args.config:
+        surok_conf = args.config
+
+f = open(surok_conf, 'r')
 conf = json.loads(f.read())
 print(conf)
 f.close()
@@ -17,14 +28,14 @@ f.close()
 
 # Get app configurations 
 def get_configs():
-    confs = [f for f in listdir(conf['confd']) if isfile(join(conf['confd'], f))]
+    confs = [f for f in listdir(conf['confd']) if isfile( join(conf['confd'], f) )]
     return confs
 
 
 # Get Surok App configuration
 def load_app_conf(app):
-    f = open(conf['confd'] + '/' + app)
-    c = json.loads(f.read())
+    f = open( conf['confd'] + '/' + app )
+    c = json.loads( f.read() )
     f.close()
 
     return c
@@ -35,12 +46,20 @@ while 1:
     confs = get_configs()
     for app in confs:
         app_conf = load_app_conf(app)
+
+        # Resolve services 
         app_hosts = resolve(app_conf, conf)
-        my = {'app': app_conf['name'], 'hosts': app_hosts}
+
+        # Populate my dictionary
+        my = { 
+                'app': app_conf['name'], 
+                'hosts': app_hosts 
+        }
+
         service_conf = gen(my, app_conf['template'])
 
-        print(reload_conf(service_conf, app_conf))
+        print( reload_conf(service_conf, app_conf) )
 
 
-    sleep(conf['wait_time'])
+    sleep( conf['wait_time'] )
 
