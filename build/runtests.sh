82940296db7ea2aefced8f513e94626c6e8d17dd
Denis Zheleztsov <d_zheleztsov@wargaming.net>
2017-03-30 17:34:26 +0300

    runtests fix

--- build/runtests.sh
+++ build/runtests.sh
@@ -7,7 +7,7 @@ set -e
 function run_tests() {
         [ -z "$(docker ps -q -f name=surok-memcache)" ] && \
         docker run -d --name surok-memcache memcached
-        docker run --rm -ti \
+        docker run --rm \
                      --link surok-memcache:memcache \
                      -v $(pwd)/tests.py:/opt/surok/tests.py \
                      -v $(pwd)/tests_entrypoint.sh:/tests_entrypoint.sh \
@@ -15,5 +15,4 @@ function run_tests() {
                      surok_base:latest
 }
 
-run_tests && docker stop -t0 surok-memcache && docker rm -f surok-memcache || \
-	OE=$?; docker stop -t0 surok-memcache && docker rm -f surok-memcache && exit $OE
+run_tests && docker stop -t0 surok-memcache && docker rm -f surok-memcache
